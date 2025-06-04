631cf21d4a9488a7a18fad0f57d81e253599cb8a
Yagya Soni <yagyasony@gmail.com>
2025-06-04 23:33:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: "polling-system-frontend-assignment-c6kqqtlub.vercel.app", // Your React app's origin
+        origin: "https://polling-system-frontend-assignment-c6kqqtlub.vercel.app/", // Your React app's origin
         methods: ["GET", "POST"]
     }
 });
